c9588296e3c7f25e9628cfaa46d4ed5f388c92ad
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-09-18 17:09:13 +0200

    cambio

---
+++ nombre.py
@@ -0,0 +1,9 @@
+entero = 10
+decimal = 3.14
+texto = "Hola, mundo"
+booleano = True
+
+print("entero:", entero, "tipo:", type(entero))
+print("decimal:", decimal, "tipo:", type(decimal))
+print("texto:", texto, "tipo:", type(texto))
+print("booleano:", booleano, "tipo:", type(booleano))
